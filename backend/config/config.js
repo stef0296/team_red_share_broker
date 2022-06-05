3b83c303f783c52ef344301cf217cef9699e57a1
stef0296 <steff.alvares@gmail.com>
2022-06-05 23:23:00 +0200

    Added environment variables

--- backend/config/config.js
+++ backend/config/config.js
@@ -4,5 +4,8 @@ dotenv.config();
 module.exports = {
     serverPort: process.env.PORT,
     mongoUrl: process.env.MONGOURL,
-    redisUrl: process.env.REDIS_URL
+    mongoDbName: process.env.MONGO_DB_NAME,
+    redisUrl: process.env.REDIS_URL,
+    alphaUrl: process.env.ALPHAURL,
+    alphaApiKey: process.env.ALPHAAPIKEY,
 }
