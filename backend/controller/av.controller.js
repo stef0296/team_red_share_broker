709d6e0a9b02a1fe18c1ed99a792bed2ab83e3e0
stef0296 <steff.alvares@gmail.com>
2022-06-07 22:37:24 +0200

    Added methods to fetch data from API

--- backend/controller/av.controller.js
+++ backend/controller/av.controller.js
@@ -3,6 +3,10 @@ const axios = require('axios').default;
 var mongoHelper = require('./mongo.controller');
 
 class AvController {
+    timeout(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
     async getNews(req, res) {
         const url = `${config.alphaUrl}?function=NEWS_SENTIMENT&apikey=${config.alphaApiKey}&topics=technology,earnings`;
         let response = await axios.get(url);
@@ -13,13 +17,32 @@ class AvController {
     }
 
     async companyOverview() {
-        let symbol = 'AAPL';
-        const url = `${config.alphaUrl}?function=OVERVIEW&apikey=${config.alphaApiKey}&symbol=${symbol}`;
+        let stocks = config.stocks;
+        for (let stock of stocks) {
+            console.log(stock);
+            const url = `${config.alphaUrl}?function=OVERVIEW&apikey=${config.alphaApiKey}&symbol=${stock}`;
+            let response = await axios.get(url);
+            let data = response.data;
+            mongoHelper.addOverviewToCollection(data);
+            await this.timeout(20000);
+        }
     }
 
     async dailyTimeSeries() {
-        let symbol = 'AAPL';
-        const url = `${config.alphaUrl}?function=TIME_SERIES_DAILY&apikey=${config.alphaApiKey}&symbol=${symbol}&outputsize=full`
+        let stocks = config.stocks; 
+        for (let stock of stocks) {
+            console.log(stock);
+            const url = `${config.alphaUrl}?function=TIME_SERIES_DAILY&apikey=${config.alphaApiKey}&symbol=${stock}&outputsize=full`
+            let response = await axios.get(url);
+            let data = response.data;
+            let timeseries = data['Time Series (Daily)'];
+            let obj = {
+                symbol: stock,
+                timeseries: timeseries
+            };
+            mongoHelper.addTimeseriesToCollection(obj);
+            await this.timeout(20000);
+        }
     }
 }
 
