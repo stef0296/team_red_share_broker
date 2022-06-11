b30702fa0e5fb388f35d94a40097d6312045f5e3
Harsha Poojari <seeker.goblet5der@gmail.com>
2022-06-11 02:04:04 +0200

    Added watchlist

--- backend/controller/mongo.controller.js
+++ backend/controller/mongo.controller.js
@@ -1,46 +1,54 @@
-var mongo = require('mongodb');
+var mongo = require("mongodb");
 // var mongoose = require('mongoose');
-var config = require('../config/config');
+var config = require("../config/config");
 var client = new mongo.MongoClient(config.mongoUrl);
 var db;
 class MongoController {
-    async init() {
-        try {
-            await client.connect();
-            console.log("Database created!");
-            db = client.db(config.mongoDbName);
-            const collection = db.collection('test01');
-        } catch (err) {
-            console.log(err);
-        }
+  async init() {
+    try {
+      await client.connect();
+      console.log("Database created!");
+      db = client.db(config.mongoDbName);
+      const collection = db.collection("test01");
+    } catch (err) {
+      console.log(err);
     }
+  }
 
-    async addNewsToCollection(data) {
-        try {
-            const collection = db.collection('news');
-            await collection.insertMany(data);
-        } catch (err) {
-            console.log(err);
-        }
+  async addNewsToCollection(data) {
+    try {
+      const collection = db.collection("news");
+      await collection.insertMany(data);
+    } catch (err) {
+      console.log(err);
     }
+  }
 
-    async addOverviewToCollection(data) {
-        try {
-            const collection = db.collection('overview');
-            await collection.insertOne(data);
-        } catch (err) {
-            console.log(err);
-        }
+  async addOverviewToCollection(data) {
+    try {
+      const collection = db.collection("overview");
+      await collection.insertOne(data);
+    } catch (err) {
+      console.log(err);
     }
+  }
 
-    async addTimeseriesToCollection(data) {
-        try {
-            const collection = db.collection('timeseries');
-            await collection.insertOne(data);
-        } catch (err) {
-            console.log(err);
-        }
+  async addTimeseriesToCollection(data) {
+    try {
+      const collection = db.collection("timeseries");
+      await collection.insertOne(data);
+    } catch (err) {
+      console.log(err);
     }
+  }
+  async addwatchlistToCollection(data) {
+    try {
+      const collection = db.collection("list");
+      await collection.insertMany(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
 }
 
 const mongoHelper = new MongoController();
--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,30 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const config = require('./config/config');
-const mongoHelper = require('./controller/mongo.controller');
-const redisHelper = require('./controller/redis.controller');
-const avHelper = require('./controller/av.controller');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const config = require("./config/config");
+const mongoHelper = require("./controller/mongo.controller");
+const redisHelper = require("./controller/redis.controller");
+const avHelper = require("./controller/av.controller");
+const watch = require("./controller/watchlist");
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-    extended: true
-}));
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    extended: true,
+  })
+);
 
-app.get('/', (_, res, next) => res.send("Team Red | Share Broker API Services"));
-
-app.get('/av-news', (req, res, next) => avHelper.getNews(req, res));
-
+app.get("/", (_, res, next) =>
+  res.send("Team Red | Share Broker API Services")
+);
+app.get("/av-news", (req, res, next) => avHelper.getNews(req, res));
+app.get("/watchlist", (req, res, next) => watch.getwatchlist(req, res));
 
 app.listen(config.serverPort, () => {
-    console.log(`App listening on port ${config.serverPort}`);
-    mongoHelper.init();
-    redisHelper.init();
+  console.log(`App listening on port ${config.serverPort}`);
+  mongoHelper.init();
+  redisHelper.init();
 });
