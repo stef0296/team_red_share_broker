2861671a14bd41d20715ebc7afd587495d4bfdd3
Pranali777 <shirsat7pranali@gmail.com>
2022-06-12 17:50:51 +0200

    Added implementation for remaining APIs

--- backend/controller/news.controller.js
+++ backend/controller/news.controller.js
@@ -2,6 +2,7 @@ const config = require('../config/config');
 const axios = require('axios').default;
 var mongoHelper = require('./mongo.controller');
 var Collection = require('../enums/collection.enum');
+const AvlDatabases = require('../enums/database.enum');
 
 class NewsController {
 
@@ -30,25 +31,38 @@ class NewsController {
 
     /// Endpoint to fetch news data from MongoDB collection
     async fetchNewsFeed(req, res) {
-        
+       let response = await this.getNewsFeedFromCollection();
+       res.send(response);
     }
 
     /// Endpoint to fetch news data for a specific ticker from MongoDB collection
-    async fetchNewsFeedForTicker(req, res) {}
+    async fetchNewsFeedForTicker(req, res) {
+        let ticker = req.query.ticker;
+        let response = await this.getTickerNewsFromCollection(ticker);
+        res.send(response);
+    }
 
     /// Database write method to add news to collection
     async addNewsToCollection(data) {
-        await mongoHelper.setData(Collection.NEWS, data, true);
+        await mongoHelper.setData(Collection.NEWS, data, true, AvlDatabases.NEWS);
     }
 
     /// Database write method to add news data for a ticker to collection
-    async addTickerNewsToCollection(ticker, data) {}
+    async addTickerNewsToCollection(ticker, data) {
+        await mongoHelper.setData(ticker, data, true, AvlDatabases.NEWS);
+    }
 
     /// Database read method to get news feed
-    async getNewsFeedFromCollection() {}
+    async getNewsFeedFromCollection() {
+        let data = await mongoHelper.getData(Collection.NEWS, {}, AvlDatabases.NEWS);
+        return(data);
+    }
 
     /// Database read method to get news feed for specific ticker
-    async getTickerNewsFromCollection(ticker) {}
+    async getTickerNewsFromCollection(ticker) {
+        let data = await mongoHelper.getData(ticker, {}, AvlDatabases.NEWS);
+        return data;
+    }
 }
 
 const newsController = new NewsController();
